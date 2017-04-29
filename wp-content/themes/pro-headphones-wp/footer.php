9a56105945ff487bbf2675ebcffcf226eee58e92
Nomi Vos <nvoswork@gmail.com>
2017-04-28 20:09:57 -0500

    activates basic theme from underscores.me

---
+++ wp-content/themes/pro-headphones-wp/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package pro-headphones-wp
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pro-headphones-wp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'pro-headphones-wp' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'pro-headphones-wp' ), 'pro-headphones-wp', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
